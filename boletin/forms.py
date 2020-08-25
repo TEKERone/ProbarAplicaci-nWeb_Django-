bc6a52083255d1312a49145da635d35a224ae6f3
Franky-code <fco.r.frias.valderrama@gmail.com>
2020-08-25 01:29:36 -0500

    Validaciones en forms

--- boletin/forms.py
+++ boletin/forms.py
@@ -6,6 +6,18 @@ class RegModelForm(forms.ModelForm):
         model = Registrado
         fields = ["nombre", "email"]
 
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        email_base, proveeder = email.split("@")
+        dominio, extesion = proveeder.split(".")
+        if not extesion == "edu":
+            raise forms.ValidationError("Utiliza un email con la extension .edu")
+        return email
+
+    def clean_nombre(self):
+        nombre = self.changed_data.get("nombre")
+        return nombre
+         
 class RegForm(forms.Form):
     nombre = forms.CharField(max_length=100)
     email = forms.EmailField()
